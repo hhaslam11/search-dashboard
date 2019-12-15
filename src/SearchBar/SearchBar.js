828460e7f8944eba2bbe669c3bd0aa7754f02c1b
hhaslam11 <kalebmarc@hotmail.com>
2019-12-14 17:55:45 -0800

    document search bar component

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import './SearchBar.scss';
 
+/**
+ * Generic search bar
+ * @param {string} props.placeholder override placeholder text | Default 'Search...'
+ * @param {boolean} props.disabled disable the search bar | Default false
+ */
 export default function SearchBar(props) {
   return (
     <input
