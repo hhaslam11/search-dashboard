bed4cf6b8f967c52684e3a380853d25d9ea82e79
hhaslam11 <kalebmarc@hotmail.com>
2019-12-19 00:07:26 -0800

    make header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,5 +7,15 @@ import './Header.scss';
  */
 export default () => {
 
-  return (<h1>header</h1>);
+  return (
+    <div className="header">
+      <div className="title">
+        <h3>JobSearch App</h3>
+        <div className="attribution">
+          <h5 className="center">Powered by </h5>
+          <a href="https://www.ziprecruiter.com/jobs" id="jobs_widget_link"><span id="zr_logo_container"><img className="center" id="zr_logo" src="https://www.ziprecruiter.com/img/logos/logo-sm-black-304px.png" alt="ZipRecruiter" width="120" /></span></a>
+        </div>
+      </div>
+    </div>
+  );
 };
