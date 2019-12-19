c7112084f8672ddc06cbf5500884e89659150b35
hhaslam11 <kalebmarc@hotmail.com>
2019-12-18 23:41:32 -0800

    add Header skeleton

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Header.scss';
+
+/**
+ * A mock/placeholder header. Not really meant to do much
+ */
+export default () => {
+
+  return (<h1>header</h1>);
+};
