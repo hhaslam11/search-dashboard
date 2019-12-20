23b4a7e51b9676018cf727bd7d79790627f8215c
hhaslam11 <kalebmarc@hotmail.com>
2019-12-20 02:22:42 -0800

    add stories for pagination

--- src/stories/Pagination.stories.js
+++ src/stories/Pagination.stories.js
@@ -1,37 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Pagination from '../Pagination/Pagination';
 
 storiesOf('Pagination', module)
   .add('no props (should be blank)', () => <Pagination/>)
-  .add('with one page', () => {
+  .add('with one page', () => <Pagination pages={1}/>)
+  .add('with 3 pages', () => <Pagination pages={3}/>)
+  .add('with pre-selected page (page 2)', () => {
     return (
       <Pagination
-      />
-    )
-  })
-  .add('with 3 pages', () => {
-    return (
-      <Pagination
-      />
-    )
-  })
-  .add('with pre-selected page', () => {
-    return (
-      <Pagination
+        pages={5}
+        value={2}
       />
     )
   })
   .add('with different render on each page', () => {
+    const [state, setState] = useState(1);
     return (
-      <Pagination
-      />
+      <>
+        <p>page {state}</p>
+        <Pagination
+          onChange={val => setState(val)}
+          pages={3}
+        />
+      </>
     )
   })
   .add('with 20 pages', () => {
+    const [state, setState] = useState(1);
     return (
-      <Pagination
-      />
+      <>
+        <p>page {state}</p>
+        <Pagination
+          onChange={val => setState(val)}
+          pages={20}
+        />
+      </>
     )
   });
