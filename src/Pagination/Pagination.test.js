f4ca4ca631a5f772d7a046542da4af593b7651ed
hhaslam11 <kalebmarc@hotmail.com>
2019-12-20 01:25:14 -0800

    add blank tests and stories

--- src/Pagination/Pagination.test.js
+++ src/Pagination/Pagination.test.js
@@ -6,3 +6,14 @@ import Pagination from './Pagination';
 test('renders without crashing', () => {
   render(<Pagination/>);
 });
+
+test.skip('should return null with invalid props', () => {});
+test.skip('should render three buttons given one page', () => {});
+test.skip('should render five buttons given three pages (prev, 1, 2, 3, next)', () => {});
+test.skip('should start one first page', () => {});
+test.skip('onChange callback should run on page change', () => {});
+test.skip('should only render up to 7 buttons at once', () => {});
+test.skip('should go to next page when "next" is clicked', () => {});
+test.skip('should go back a page when "prev" is clicked', () => {});
+test.skip('prev and next buttons should be disabled if only one page', () => {});
+test.skip('current page should be disabled', () => {});
--- src/stories/Pagination.stories.js
+++ src/stories/Pagination.stories.js
@@ -4,4 +4,28 @@ import { storiesOf } from '@storybook/react';
 import Pagination from '../Pagination/Pagination';
 
 storiesOf('Pagination', module)
-  .add('basic', () => <Pagination/>);
+  .add('no props (should be blank)', () => <Pagination/>)
+  .add('with one page', () => {
+    return (
+      <Pagination
+      />
+    )
+  })
+  .add('with 3 pages', () => {
+    return (
+      <Pagination
+      />
+    )
+  })
+  .add('with different render on each page', () => {
+    return (
+      <Pagination
+      />
+    )
+  })
+  .add('with 20 pages', () => {
+    return (
+      <Pagination
+      />
+    )
+  });
